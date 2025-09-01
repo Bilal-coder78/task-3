ddf34f205f2631e2a107f97e029ea4910145e611
M Bilal <bilalsikander87@gmail.com>
2025-09-01 14:44:52 +0500

    client sec complete

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import About from './About/About';
 import './App.css';
 import BLuebox from './Bluebox/BLuebox';
 import Card from './Cards/Card';
+import Client from './Clients/Client';
 import Home from './Home/Home';
 import Mobile from './Mobile/Mobile';
 import Nav from './Navbar/Nav';
@@ -15,6 +16,7 @@ function App() {
       <Card/>
       <Mobile/>
       <BLuebox/>
+      <Client/>
     </>
   );
 }
---
+++ src/Clients/Client.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import "./client.css"
+import client1 from "../assets/clients/client-1.png"
+import client2 from "../assets/clients/client-2.png"
+import client3 from "../assets/clients/client-3.png"
+import client4 from "../assets/clients/client-4.png"
+import client5 from "../assets/clients/client-5.png"
+import client6 from "../assets/clients/client-6.png"
+import { GoDotFill } from "react-icons/go";
+
+function Client() {
+  return (
+    <div className='container mb-5'style={{marginTop:"5rem"}}>
+        <div class="row gap-4">
+        <div class="col-12 clients flex-wrap d-flex align-items-center justify-content-center gap-sm-5 gap-3">
+           <img src={client1} alt="" className='client'/>
+           <img src={client2} alt="" className='client'/>
+           <img src={client3} alt="" className='client d-none d-sm-block'/>
+           <img src={client4} alt="" className='client d-none d-sm-block'/>
+           <img src={client5} alt="" className='client d-none d-sm-block'/>
+           <img src={client6} alt="" className='client d-none d-sm-block'/>
+        </div>
+        <div class="col-12 dots d-flex align-items-center justify-content-center gap-1">
+            <GoDotFill className='fw-bold fs-4 pointer'/>
+            <GoDotFill className='fw-bold fs-4 pointer'/>
+            <GoDotFill className='fw-bold fs-4 pointer'/>
+            <GoDotFill className='fw-bold fs-4 pointer'/>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Client
