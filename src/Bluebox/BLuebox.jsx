c92cdc6ff1181ee22b493c3cb7a30f2799b4636f
M Bilal <bilalsikander87@gmail.com>
2025-09-02 15:59:11 +0500

    blueboxes dynamic

--- src/Bluebox/BLuebox.jsx
+++ src/Bluebox/BLuebox.jsx
@@ -2,14 +2,19 @@ import React from 'react'
 import "./bluebox.css"
 import { Link } from 'react-scroll'
 
-function BLuebox() {
+function BLuebox({
+   title = "Maecenas tempus tellus eget condimentum",
+  description = "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel dgfudis adfuga gua",
+  buttonText = "Call To Action",
+  linkTo = "home",
+}) {
   return (
-    <div className="container-fluid container-sm mb-5"style={{marginTop:"6rem"}}>
+    <div className="container mb-5"style={{marginTop:"6rem"}}>
         <div className="bluebox d-flex align-items-center justify-centent-center w-100 rounded-4">
             <div className="box-content text-white px-5 text-center">
-                <h4 className='fs-1 fw-light my-sm-4 mb-3'>Maecenas tempus tellus eget condimentum</h4>
-                <p className='my-sm-4 fw-light'>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel dgfudis adfuga gua</p>
-                <Link to='home' className="button"><button className='my-sm-4'>Call To Action</button></Link>
+                <h4 className='fs-1 fw-light my-sm-4 mb-3'>{title}</h4>
+                <p className='my-sm-4 fw-light'>{description}</p>
+                <Link to={linkTo} className="button"><button className='my-sm-4'>{buttonText}</button></Link>
             </div>
         </div>
     </div>
--- src/Bluebox/BLuebox2.jsx
+++ src/Bluebox/BLuebox2.jsx
@@ -2,16 +2,23 @@ import React from 'react'
 import "./bluebox.css"
 import { Link } from 'react-scroll'
 
-function Bluebox2() {
+function Bluebox2({
+
+  title="Call To Action",
+  description="Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel dgfudis adfuga gua",
+  btntext="Call To Action",
+  linkto="home"
+})
+ {
   return (
     <div className="" style={{ backgroundColor: "#f4f9fd" }}>
       <div className="box_2">
         <div className="container-fluid mb-5" style={{ marginTop: "5rem" }}>
           <div className="bluebox d-flex align-items-center justify-centent-center w-100 rounded-4">
             <div className="box-content text-white px-5 text-center gap-3">
-              <h2 className='fs-1 fw-semibold my-sm-4 mb-3'>Call To Action</h2>
-              <p className='my-sm-4 fw-medium'>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel dgfudis adfuga gua</p>
-              <Link to='home' className="button"><button className='my-sm-4'>Call To Action</button></Link>
+              <h2 className='fs-1 fw-semibold my-sm-4 mb-3'>{title}</h2>
+              <p className='my-sm-4 fw-medium'>{description}</p>
+              <Link to={linkto} className="button"><button className='my-sm-4'>{btntext}</button></Link>
             </div>
           </div>
         </div>
